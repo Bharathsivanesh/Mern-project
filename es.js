3f20c4bafcb7a92043217df8be3f8e2065454ace
Bharath Sivanesh.S <132342144+Bharathsivanesh@users.noreply.github.com>
2024-09-16 14:33:35 +0530

    promise

--- es.js
+++ es.js
@@ -31,11 +31,40 @@
 // console.log(apijson);
 // return apijson;
 
-async function method() {
-  const api = await fetch("https://66e526d55cc7f9b6273c69d9.mockapi.io/users");
-  const apijson = await api.json();
-  console.log(apijson);
-  return apijson;
+// async function method() {
+//   const api = await fetch("https://66e526d55cc7f9b6273c69d9.mockapi.io/users");
+//   const apijson = await api.json();
+//     // console.log(apijson);
+//   return apijson;
+// }
+// async function filterdata()
+// {
+// const a=await method();
+// const ans=a.filter(data=>data.isActive);
+// console.log(ans);
+// }
+// filterdata();
+//promises using then and catch
+
+function pro()
+{
+  return new Promise((resolve,reject)=>{
+    let sucess=true;
+    setTimeout(()=>{
+      if(sucess)
+      {
+        resolve("sucess");
+      }
+      else{
+        reject("reject");
+      }
+    },2000)
+  })
 }
-const a=method();
-console.table(a);
+pro().then((result)=>{
+  console.log(result)
+}).catch((error)=>{
+console.log(error);
+})
+
+
